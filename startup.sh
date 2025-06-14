4801e8d0b937620435ba17c9f0f6ea782d614d17
Abdelrhman Ahmed <100162976+AbdoAhmed666@users.noreply.github.com>
2025-06-14 18:39:32 +0300

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment if exists (optional)
-# source /home/site/wwwroot/venv/bin/activate
-
-# Run with python -m streamlit to avoid PATH issues
-python -m streamlit run web/application.py --server.port $PORT --server.enableCORS false
