09c7fa06c50246f6f3387bc2f72a9f145cd7a015
Abdelrhman Ahmed <100162976+AbdoAhmed666@users.noreply.github.com>
2025-06-14 18:28:47 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-streamlit run web/application.py --server.port \$PORT --server.enableCORS false
+
+# Activate the virtual environment if exists (optional)
+# source /home/site/wwwroot/venv/bin/activate
+
+# Run with python -m streamlit to avoid PATH issues
+python -m streamlit run web/application.py --server.port $PORT --server.enableCORS false
